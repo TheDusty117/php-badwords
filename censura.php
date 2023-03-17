ccc72d4ebc2c2e1285ab2787aa849aa0b153db3a
TheDusty117 <gabrieledipassio@gmail.com>
2023-03-17 16:05:26 +0100

    FEAT successfully censored

--- censura.php
+++ censura.php
@@ -9,13 +9,16 @@ var_dump($_GET);
 
 
 
-$p =$scritto;
-var_export($p);
+$esplicito = $scritto;
+var_dump($esplicito);
+
+$censura = str_replace($da_censurare, '***', $esplicito);
+
 
 
 ?>
 
-<h1>Frase Originale: <?php echo $scritto ?> </h1>
+<h2>Frase Originale: <?php echo $scritto ?> </h2>
+<h2>Parola da Censurare: <?php echo $da_censurare ?> </h2>
 
-
-<h1>Frase con parola Censurata  </h1>
+<h1>Frase con parola Censurata: <?php echo $censura ?>  </h1>
